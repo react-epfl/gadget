c6abf81839694b0bf5a30aa18916b45e07d42095
Evgeny Bogdanov <evgeny.bogdanov@epfl.ch>
2012-10-29 14:48:14 +0000

    production updaptation

--- prod/grid_view/app.js
+++ prod/grid_view/app.js
@@ -22,7 +22,7 @@ var isOwner = false;
 var my = {};
 var app = { context: "", viewerName: ""
           , data: { view: "" }
-          , root_url: "http://localhost/gadget/test/grid_view/"
+          , root_url: "http://graasp.epfl.ch/gadget/prod/grid_view/"
           }
 
 // gets the data and calls build for container
