7b6cd89590068396655c770fc4b7a52f852845ec
Evgeny Bogdanov <evgeny.bogdanov@gmail.com>
2013-03-19 13:04:35 +0100

    remove changes for context

--- prod/grid_view/app.js
+++ prod/grid_view/app.js
@@ -58,13 +58,12 @@ var initialize = function() {
     var appdata = data.appdata // .settings
     var apps = data.apps // .list
 
-    // add space title
-    $("#title").append(" "+context.displayName)
-
     // current viewer is the owner, then show management block
     if (app.viewer.id === app.owner.id) {
       isOwner = true
     }
+
+    console.log(context)
 
     // --- apps from space ---
     app.list = apps.list
