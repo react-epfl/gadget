4cfba5867603b80a88ae927b7823827341f725af
Wissam-Halimi <wissam.halimi@epfl.ch>
2014-01-16 11:42:12 +0100

    Reading coordinates from file. Polishing some features. Formatting. Cleaning code.

--- sismondi/ConversionOfCoordinates.java
+++ sismondi/ConversionOfCoordinates.java
@@ -15,9 +15,10 @@ public class ConversionOfCoordinates
 
 		try{
 			FileReader file = new FileReader("/Users/wissam/gadget/sismondi/satellite3d.txt");
-			PrintWriter writer = new PrintWriter("/Users/wissam/gadget/sismondi/satellite2d.txt");
+			PrintWriter writerAll = new PrintWriter("/Users/wissam/gadget/sismondi/satellite2d.txt");
+			PrintWriter writer = new PrintWriter("/Users/wissam/gadget/sismondi/plot.txt");
 			BufferedReader br = new BufferedReader(file);
-			String line = new String();//br.readLine();
+			String line = new String();
 			String delim = "[ ]+";
 
 			//Parameters from the file
@@ -30,23 +31,24 @@ public class ConversionOfCoordinates
 			ArrayList<Double> x_n = new ArrayList<Double>();
 			ArrayList<Double> y_n = new ArrayList<Double>();
 
-			//line = br.readLine();
 			while((line = br.readLine())!=null){
 				String[] tokens = line.split(delim);
-				// System.out.println(line);
 				t.add(tokens[0]);
 				x.add(Double.parseDouble(tokens[1]));
 				y.add(Double.parseDouble(tokens[2]));
 				z.add(Double.parseDouble(tokens[3]));
-				//line = br.readLine();
 			}
 
+			writer.print("[");
 			for(int i=0;i<t.size();i++){	
 				float xn = (float)(a*(x.get(i) - y.get(i)));
 				float yn = (float)(b*z.get(i) - c*(x.get(i) + y.get(i)));
-				writer.print(t.get(i) + " " + xn + " " + yn + "\n");
+				writerAll.print(t.get(i) + " " + xn + " " + yn + "\n");
+				writer.print("[" + xn + "," + yn + "],");
 			}
+			writer.print("]");
 			writer.close();
+			writerAll.close();
 			br.close();
 
 		}catch(FileNotFoundException exception){
@@ -57,7 +59,6 @@ public class ConversionOfCoordinates
 	}
 public static void main(String args[])
 	{
-		System.out.println("Hello");
 		ConversionOfCoordinates con = new ConversionOfCoordinates();
 		con.convert();
 	}
