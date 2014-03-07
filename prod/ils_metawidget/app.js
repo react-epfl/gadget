70963b80f7d3c873d23f78da0aacfb4e6aaefab2
alextrichos <alexandros.trichos@gmail.com>
2014-03-07 15:12:21 +0200

    Changes in order to reflect the latest i18n updates

--- prod/ils_metawidget/app.js
+++ prod/ils_metawidget/app.js
@@ -4,7 +4,8 @@ var app = { context: "", viewerName: ""
           , data: { view: "" }
           , root_url: "http://graasp.epfl.ch/gadget/prod/ils_metawidget/"
           , user_name: ""
-          };
+          , prefs: new gadgets.Prefs()
+          }
 
 // gets the data and calls build for container
 var initialize = function() {
@@ -24,33 +25,33 @@ var initialize = function() {
   shindig.container.layoutManager = new my.LayoutManager();
   // to remove building prefs and title block in gadget
   shindig.Gadget.prototype.getContent=function(A){
-    shindig.callAsyncAndJoin(["getMainContent"],function(B){A(B.join(""));},this);
-  };
+    shindig.callAsyncAndJoin(["getMainContent"],function(B){A(B.join(""))},this)
+  }
 
   // make toolbar togglable
   $("#tools_title").click(function() {
     var tools_panel = document.getElementById("tools_content");
     var main_block = document.getElementById("main_block");
     if ((tools_panel.style.display == 'none') || (tools_panel.style.display == '')) {
+      $("#tools_content").show();
       $("#arrow_down").show();
       $("#arrow_up").hide();
       if(app.list.length > 0) {
         main_block.style.bottom = "500px";
-        $("#tools_content").show();
       }
     } else {
+      $("#tools_content").hide();
       $("#arrow_down").hide();
       $("#arrow_up").show();
       if(app.list.length > 0) {
         main_block.style.bottom = "40px";
-        $("#tools_content").hide();
       }
     }
   });
 
   //getting the user's settings
   getData(function (data) {
-    app.viewer = data.viewer; // .displayName
+    app.viewer = data.viewer // .displayName
     app.viewerName = data.viewer.displayName;
     var context = data.context; // .contextId, .contextType
     app.context = context;
@@ -203,11 +204,10 @@ var remove_hidden_spaces = function(subspaces) {
 
 // identify which user is using this url
 var identifyUser = function() {
-   var prefs = new gadgets.Prefs();
   // check if the cookie exists, if not, set the cookie
   if ($.cookie('graasp_user')) {
     app.user_name = $.cookie('graasp_user');
-    $('#hello_msg').text(prefs.getMsg("hello") + " " + app.user_name + "!");
+    $('#hello_msg').text(app.prefs.getMsg("hello") + " " + app.user_name + "!");
     updateUserActions(app.user_name);
   } else {
     $('#login_popup').modal('show');
@@ -223,7 +223,6 @@ var identifyUser = function() {
 
 // save user's name in appData and display user name on the page
 var saveUserName = function() {
-	var prefs = new gadgets.Prefs();
   app.user_name = $('#user_name').val();
   if (!app.user_name || /^\s*$/.test(app.user_name) || 0 === app.user_name.length) {
     $("#error_msg").show();
@@ -231,7 +230,7 @@ var saveUserName = function() {
     updateUserActions(app.user_name);
     $.cookie('graasp_user', app.user_name, { expires: 1 });
     $('#login_popup').modal('hide');
-    $('#hello_msg').text(prefs.getMsg("hello") + " " + app.user_name + "!");
+    $('#hello_msg').text(app.prefs.getMsg("hello") + " " + app.user_name + "!");
   }
 };
 
@@ -245,147 +244,147 @@ var updateUserActions = function(user_name) {
   osapi.appdata.update(
     { userId: app.contextId,
       data: user_hash
-    }).execute(function() {});
-};
+    }).execute(function() {})
+}
 
 // sets app sizes based on template
 // always does it in %
 var setViewSize = function (view) {
-  var arr = [];
+  var arr = []
   switch (view) {
     case '1':
-      arr = [99]; break;
+      arr = [99]; break
     case '2':
-      arr = [99, 99, 49.5]; break;
+      arr = [99, 99, 49.5]; break
     case '3':
-      arr = [99, 49.5]; break;
+      arr = [99, 49.5]; break
     case '4':
-      arr = [99, 33]; break;
+      arr = [99, 33]; break
     case '5':
-      arr = [49.5]; break;
+      arr = [49.5]; break
     case '6':
-      arr = [33]; break;
+      arr = [33]; break
   }
-  var arrLength = arr.length;
-  var last = arr[arrLength - 1];
+  var arrLength = arr.length
+  var last = arr[arrLength - 1]
   _.each(app.order, function (id, i) {
     // for first items take from template, for the rest - all the same
-    var size = (i < arrLength) ? arr[i] : last;
-    app.sizes[id] = size;
-  });
-};
+    var size = (i < arrLength) ? arr[i] : last
+    app.sizes[id] = size
+  })
+}
 // builds sizes as % or px depending on chosen type
 // and update the app.sizes accordingly
 var rebuildSizes = function (parent) {
-  var centerSize = $("#center").width();
+  var centerSize = $("#center").width()
   parent.find(".window").each(function (i) {
     var win = $(this)
       , appId = win.attr("appId")
       , size = win.width()
-      , finalSize;
+      , finalSize
     if (app.sizeType === "%") {
-      finalSize = size*100/centerSize;
-      win.css("width", finalSize+"%");
-      app.sizes[appId] = finalSize;
+      finalSize = size*100/centerSize
+      win.css("width", finalSize+"%")
+      app.sizes[appId] = finalSize
     } else {
       //finalSize = size*centerSize/100
-      finalSize = size;;
-      win.css("width", finalSize+"px");
-      app.sizes[appId] = finalSize;
+      finalSize = size
+      win.css("width", finalSize+"px")
+      app.sizes[appId] = finalSize
     }
-  });
-};
+  })
+}
 // type = % or px
 var resizeAllApps = function (type) {
-  type = type || app.sizeType;
-  var sizes = app.sizes;
+  type = type || app.sizeType
+  var sizes = app.sizes
   $("#center").find(".window").each(function (i) {
-    var win = $(this);
-    var appId = win.attr('appId');
-    win.css("width", sizes[appId]+type);
-  });
-};
+    var win = $(this)
+    var appId = win.attr('appId')
+    win.css("width", sizes[appId]+type)
+  })
+}
 // refreshes order of app, takes as the base appdata representation
 // removes deleted app and adds new apps
 var refreshAppsList = function (app_json) {
   // removes apps that are no longer in the space
-  var savedIds = []; // list of valid ids that are in the app.order
+  var savedIds = [] // list of valid ids that are in the app.order
   _.each(app_json.order, function (id, i) {
     if (!app_json.hash[id]) { // delete id since it does not exist anymore
-      delete app_json.order[i];
-      delete app_json.sizes[id];
+      delete app_json.order[i]
+      delete app_json.sizes[id]
     } else { // add to the current savedIds list
-      savedIds.push(id);
+      savedIds.push(id)
     }
-  });
+  })
 
   // appends new space apps to the end
-  var curIds = _.keys(app_json.hash);
-  var newIds = _.difference(curIds, savedIds);
+  var curIds = _.keys(app_json.hash)
+  var newIds = _.difference(curIds, savedIds)
   _.each(newIds, function (id) {
-    app_json.order.push(id);
-    app_json.sizes[id] = 300; // 300 - is the default width
-  });
+    app_json.order.push(id)
+    app_json.sizes[id] = 300 // 300 - is the default width
+  })
 
-  save(true,app);
-};
+  save(true,app)
+}
 
 var adjustHeight = function () {
   gadgets.window.adjustHeight();
-};
+}
 
 var buildSkeleton = function (container,app_json, is_center) {
   // build first drop_here block
   var fakeGadget = $('<div id="fake_gadget" appId="0"></div>')
-    .append($('<div class="drop_here"></div>'));
-  container.append(fakeGadget);
+    .append($('<div class="drop_here"></div>'))
+  container.append(fakeGadget)
 
   // build apps
   _.each(app_json.order, function (id) {
-    buildWindow(id, container, app_json, is_center);
-  });
+    buildWindow(id, container, app_json, is_center)
+  })
   // resize width of apps
-  resizeAllApps();
+  resizeAllApps()
 
   // //set fake gadget height to the height of the first app
-  fakeGadget.height(container.find(".window").height());
+  fakeGadget.height(container.find(".window").height())
 
   $(".window").draggable(
     { revert: "invalid"
     , start: function (ev, ui) {
-        var g = $(ev.target);
-        $(".window_placeholder").addClass("active");
-        g.css("zIndex", 10);
+        var g = $(ev.target)
+        $(".window_placeholder").addClass("active")
+        g.css("zIndex", 10)
       }
     , drag: function (ev, ui) {
         // disable the droppable before dragged item
-        $(ev.target).prev().find(".drop_here").removeClass("active");
+        $(ev.target).prev().find(".drop_here").removeClass("active")
       }
     , stop: function (ev, ui) {
-        $(".window_placeholder").removeClass("active");
-        $(ev.target).css("zIndex", "auto");
+        $(".window_placeholder").removeClass("active")
+        $(ev.target).css("zIndex", "auto")
       }
-   });
+    })
   $(".window").resizable(
     { handles: "e"
     , maxWidth: "100%"
     , containement: "center"
     , start: function (ev, ui) {
-        $(".window_placeholder").addClass("active");
-        ui.element.css("position", "relative").css("left", "0").css("top", "0");
+        $(".window_placeholder").addClass("active")
+        ui.element.css("position", "relative").css("left", "0").css("top", "0")
       }
     , resize: function (ev, ui) {
-        ui.element.css("position", "relative").css("left", "0").css("top", "0");
+        ui.element.css("position", "relative").css("left", "0").css("top", "0")
       }
     , stop: function (ev, ui) {
-        $(".window_placeholder").removeClass("active");
+        $(".window_placeholder").removeClass("active")
         // save new width
-        var appId = ui.element.attr('appId');
+        var appId = ui.element.attr('appId')
 
-        rebuildSizes(ui.element.parent());
-        save(app);
-     }
-   });
+        rebuildSizes(ui.element.parent())
+        save(app)
+      }
+    })
 
   $(".drop_here").droppable(
     { hoverClass: "hover"
@@ -393,65 +392,61 @@ var buildSkeleton = function (container,app_json, is_center) {
     , tolerance: "pointer"
     , drop: function (ev, ui) {
         // insert dragged item after current droppable
-        var prev = $(this).parent();
-        var prevId = prev.attr('appId');
-        var cur = ui.draggable;
-        var curId = cur.attr('appId');
+        var prev = $(this).parent()
+          , prevId = prev.attr('appId')
+          , cur = ui.draggable
+          , curId = cur.attr('appId')
 
-        prev.after(ui.draggable);
+        prev.after(ui.draggable)
         // set left and top to 0 for dragged item
-        cur.css('left',0).css('top',0);
+        cur.css('left',0).css('top',0)
         // update order array
-        app_json.order.splice(_.indexOf(app_json.order, curId), 1);
-        var prevPos = (prevId == 0) ? 0 : (_.indexOf(app_json.order, prevId)+1);
-        app_json.order.splice(prevPos, 0, curId);
+        app_json.order.splice(_.indexOf(app_json.order, curId), 1)
+        var prevPos = (prevId == 0) ? 0 : (_.indexOf(app_json.order, prevId)+1)
+        app_json.order.splice(prevPos, 0, curId)
         // build gadget content again (iframe is lost for some reason)
-        buildGadget(curId, app_json, is_center);
+        buildGadget(curId, app_json, is_center)
         // save new position
-        save();
+        save()
       }
-   });
-};
+    })
+}
 
 var buildWindow = function (id, parent, app_json, is_center) {
-  var gadget = app_json.hash[id];
+  var gadget = app_json.hash[id]
 
   // build placeholder
   var blk = $("<div></div>")
     .addClass("window")
-    .attr('appId', gadget.id);
+    .attr('appId', gadget.id)
 
   var title = $("<div></div>").addClass('gadgets-gadget-title-bar')
-    .append($("<span></span>").text(gadget.displayName));
-  blk.append(title);
-  parent.append(blk);
+    .append($("<span></span>").text(gadget.displayName))
+  blk.append(title)
+  parent.append(blk)
 
-  var gadget_el = $("<div></div>").attr('id', 'gadget-chrome-'+id);
-  blk.append(gadget_el);
-  buildGadget(id, app_json, is_center);
+  var gadget_el = $("<div></div>").attr('id', 'gadget-chrome-'+id)
+  blk.append(gadget_el)
+  buildGadget(id, app_json, is_center)
 
   blk.append($('<div class="window_placeholder"></div>'));
   blk.append($('<div class="drop_here"></div>'));
-};
+}
 
 // is_center indicates if the gadget is in the center or at the bottom tool bar
 var buildGadget = function (id, app_json, is_center) {
   var gadget = app_json.hash[id];
-  try {
-    var prefs = new gadgets.Prefs();
-    var lang = prefs.getLang(); //get the language
-    var country = prefs.getCountry(); //and the country
-    shindig.container.setLanguage(lang); // set the language to shingig
-    shindig.container.setCountry(country); // and the country
-    } catch(err){
-        console.log("Cannot get language/country");
-    };
+  var lang = app.prefs.getLang(); //get the language
+  var country = app.prefs.getCountry(); //and the country
+  shindig.container.setLanguage(lang); // set the language to shingig
+  shindig.container.setCountry(country); // and the country
+
   // get secure token for each widget from osapi.apps request
   var gadgetParams =
     { specUrl: gadget.appUrl
     , appId: id
     , secureToken: gadget.token
-    };
+    }
   // for gadgets in the center, use the height of the gadgets themselves
   // for gadgets at the bottom tool bar, set height as 400px
   var gadget_size = {};
@@ -464,10 +459,10 @@ var buildGadget = function (id, app_json, is_center) {
   }else{
     gadgetParams['height'] = '400px';
   }
-  var gadgetEl = shindig.container.createGadget(gadgetParams);
+  var gadgetEl = shindig.container.createGadget(gadgetParams)
   // if no token specified, make it anonymous by removing secureToken
   if (!gadget.token) {
-    delete gadgetEl.secureToken;
+    delete gadgetEl.secureToken
   }
   shindig.container.addGadget(gadgetEl);
 
@@ -490,7 +485,7 @@ var buildGadget = function (id, app_json, is_center) {
 
   shindig.container.setView("home");
   shindig.container.renderGadget(gadgetEl);
-};
+}
 
 // get the size of gadget before rendering it
 var getGadgetSize = function(gadgetUrl){
@@ -498,16 +493,16 @@ var getGadgetSize = function(gadgetUrl){
   var shindig_context = {
     "view": "canvas",
     "container": "default"
-  };
+  }
   var shindig_gadgets = new Array();
   shindig_gadgets[0] = {
     "url": gadgetUrl,
     "moduleId": 0
-  };
+  }
   var shindig_data = {
     "context": shindig_context,
     "gadgets": shindig_gadgets
-  };
+  }
   str_data = JSON.stringify(shindig_data);
 
   xhr.open( "POST", "http://shindig.epfl.ch:80/gadgets/metadata?st=0:0:0:0:0:0:0", false );
@@ -522,31 +517,31 @@ var getGadgetSize = function(gadgetUrl){
   var w_gadget = shindig_response['gadgets'][0]['width']?shindig_response['gadgets'][0]['width']:"";
   var s_gadget = {
     gadgetHeight: h_gadget,
-    gadgetWidth: w_gadget,
-  };
+    gadgetWidth: w_gadget
+  }
   return s_gadget;
-};
+}
 
 // notHumanAct - save that is not initiated by a human
 var save = function(notHumanAct, app_json){
-  var humanAct = !notHumanAct;
+  var humanAct = !notHumanAct
   //only owner can save changes
   if (humanAct && !isOwner) {
-    $("#not_owner").show();
-    return;
+    $("#not_owner").show()
+    return
   }
   if (isFreeze) {
-    return;
+    return
   }
-  var data = {};
-  data.order = app_json.order;
-  data.sizes = app_json.sizes;
-  data.sizeType = app_json.sizeType;
+  var data = {}
+  data.order = app_json.order
+  data.sizes = app_json.sizes
+  data.sizeType = app_json.sizeType
 
   osapi.appdata.update(
     { userId: app_json.contextId
     , data: {"settings": JSON.stringify(data)}
     })
-    .execute(function() {});
-};
+    .execute(function() {})
+}
 
