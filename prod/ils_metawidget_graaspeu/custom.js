35a5309dc97e7ea09e875a8686b7d33ef0ab8df2
alextrichos <alexandros.trichos@gmail.com>
2014-10-10 19:02:33 +0300

    UI Enhancements: - ILS can handle many tabs. - Description block is collapsible. - Tab bar is attached under the info bar. - Misc minor enhancements and fixes.

--- prod/ils_metawidget_graaspeu/custom.js
+++ prod/ils_metawidget_graaspeu/custom.js
@@ -8,34 +8,113 @@ var applyNewLayout= function  () {
    $("#ils_phases .tab-pane").addClass("fade"); //Add Fade effect to tabs
    $("#ils_phases .tab-pane:first").addClass("in"); //Fade the tab in
    $("#ils_cycle").wrap("<div id='customTopWrapper' class='customTop'></div>");
-   //$("#customTopWrapper").prepend("<div id='leftChevron' class='tabChevron'/><div id='righrChevron' class='tabChevron'/>");
 
 
     //When done, wait 1000ms hide the loader and display content
     setTimeout(function(){
         $("#main").show("fade",1000);
         $("#loader").hide("fade",500).remove();
-        $("body").addClass("bg_image");
+        $("body").addClass("bg_image")
+        checkTabBarOverflow();
     },1000);
 
 
     myNavMenu = $('#customTopWrapper');  //gets the tabbed menu
     customBar =$('#myCustomBar'); //get the info bar
+    customBarWrapper =$('#customBarWrapper'); //get the customBarWrapper
     faux = $('#fauxContainer'); //gets the faux container
     customBarHeight = customBar.outerHeight(); //gets the height of info bar
     myNavMenuHeight = myNavMenu.outerHeight(); //gets the height of tabbed menu
     attached = false;
     windowScrollTop= $(window).scrollTop(); //gets the ammount of scrolling in main_block
 
+
     $('body').on('click', '.fixedCustomTop', function (e) {
         $(window).scrollTop(fauxOffset+2);
     });
+
+    $('body').on('click', '#title', function (e) {
+        setTimeout(function(){stickyBar();},50);
+        $("#description_block").slideToggle(500,function(){stickyBar();});
+    });
+
+
 
     $('#logout_button').on("click",function(e){
        logoutUser();
     });
 
 };
+
+var checkTabBarOverflow=function(){ //Check if tab bar has overflow
+    var ils_cycle_bar=$("#ils_cycle");
+    var ils_wrapper=$('#customTopWrapper');
+    if (checkOverflow()){
+        ils_cycle_bar.addClass("hasChevrons").parent("#customTopWrapper").prepend("<div id='leftChevron' class='tabChevron left disabled'/><div id='rightChevron' class='tabChevron right'/>");
+
+        $('body')
+            .on('click','#leftChevron',function (e) {
+                var elem=hasHidden(this);
+                if (elem){
+                    $("#rightChevron").removeClass("disabled");
+                    elem.show();
+                }
+                if (!hasHidden(this)){
+                    $(this).addClass("disabled");
+                }
+                e.stopPropagation();
+            })
+            .on('click','#rightChevron',function (e) {
+                var firstVisible=$(this).nextAll("#ils_cycle").children(":visible:first");
+                if (checkOverflow()){
+                    firstVisible.hide();
+                    $("#leftChevron").removeClass("disabled");
+                }
+                if (!checkOverflow()){
+                    $(this).addClass("disabled");
+                }
+                e.stopPropagation();
+            })
+            .on('tabClick',function (e) {
+                var clickedTab = isOutsideContainer(e);
+                if (clickedTab){
+                        $("#rightChevron").trigger('click');
+                        $(e.target).trigger("tabClick");
+
+                }
+            });
+    }
+
+    function checkOverflow(){ //checks if tab bar has still overflowing items to display (on the right)
+
+        var childrenWidth=0;
+        ils_cycle_bar.children(':visible').each(function(){
+            childrenWidth+=$(this).width();
+        });
+        var margin=3;
+        if (childrenWidth-margin > ils_cycle_bar.width()) {
+        return true;
+        }else return false;
+    }
+
+    function hasHidden(elem){ // checks if tab bar has hidden  items to display (on the left)
+        var hiddenElem=$(elem).nextAll("#ils_cycle").children(":hidden:last");
+        if (hiddenElem[0]!=null && hiddenElem[0]!=undefined&hiddenElem[0]!=""){
+            return hiddenElem;
+        }else return false;
+    }
+
+    function isOutsideContainer(elem){
+        var targetTab= $(elem.target).parent();
+        var targetTabWidth=targetTab.outerWidth();
+        var targetTabOffset=targetTab.position().left;
+        var containerWidth=targetTab.parents("#ils_cycle").width();
+        if ((targetTabOffset+targetTabWidth)>containerWidth) {
+            return targetTab;
+        }
+        else return false;
+    }
+}
 
 
 $(window).scroll(function(){
@@ -51,18 +130,17 @@ function stickyBar() {
     var test2 = fauxOffset - windowScrollTop - customBarHeight;
     myNavMenuHeight = myNavMenu.outerHeight(); //gets the height of tabbed menu
 
-    if ((test < 0) && !attached ) //attach the sticky bar when it has reached to the top
+    if ((test < customBarHeight) && !attached ) //attach the sticky bar when it has reached to the top
         {
             attached=true;
-            myNavMenu.addClass("fixedCustomTop");
+            myNavMenu.addClass("fixedCustomTop").css("top",customBarHeight);
             faux.css("height", myNavMenuHeight);
-            myNavMenu.fadeIn("slow");
          }
 
-    else   if ((test > 0) && attached) // detach the sticky bar when not at the top
+    else   if ((test > customBarHeight) && attached) // detach the sticky bar when not at the top
 
     {
-        myNavMenu.removeClass("fixedCustomTop")
+        myNavMenu.removeClass("fixedCustomTop").css("top",0);
         faux.css("height", 0);
         attached=false;
     }
