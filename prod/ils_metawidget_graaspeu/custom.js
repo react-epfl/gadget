bf874467b6ac6e83e2e8dffbdaf57ec9688d8102
María Jesús Rodríguez Triana <maria.rodrigueztriana@epfl.ch>
2014-10-27 16:12:05 +0100

    Revert "ILS Tablet/Mobile Enhancements & minor fixes:"

--- prod/ils_metawidget_graaspeu/custom.js
+++ prod/ils_metawidget_graaspeu/custom.js
@@ -26,12 +26,11 @@ var applyNewLayout= function  () {
     customBarHeight = customBar.outerHeight(); //gets the height of info bar
     myNavMenuHeight = myNavMenu.outerHeight(); //gets the height of tabbed menu
     attached = false;
-
+    windowScrollTop= $(window).scrollTop(); //gets the ammount of scrolling in main_block
 
 
     $('body').on('click', '.fixedCustomTop', function (e) {
-        var descriptionOffset=$("#description_block").innerHeight();
-        $("#main_block").scrollTop(descriptionOffset);
+        $(window).scrollTop(fauxOffset+2);
     });
 
     $('body').on('click', '#title', function (e) {
@@ -39,38 +38,106 @@ var applyNewLayout= function  () {
         $("#description_block").slideToggle(500,function(){stickyBar();});
     });
 
+
+
     $('#logout_button').on("click",function(e){
        logoutUser();
     });
 
 };
 
+var checkTabBarOverflow=function(){ //Check if tab bar has overflow
+    var ils_cycle_bar=$("#ils_cycle");
+    var ils_wrapper=$('#customTopWrapper');
+    if (checkOverflow()){
+        ils_cycle_bar.addClass("hasChevrons").parent("#customTopWrapper").prepend("<div id='leftChevron' class='tabChevron left disabled'/><div id='rightChevron' class='tabChevron right'/>");
+
+        $('body')
+            .on('click','#leftChevron',function (e) {
+                var elem=hasHidden(this);
+                if (elem){
+                    $("#rightChevron").removeClass("disabled");
+                    elem.show();
+                }
+                if (!hasHidden(this)){
+                    $(this).addClass("disabled");
+                }
+                e.stopPropagation();
+            })
+            .on('click','#rightChevron',function (e) {
+                var firstVisible=$(this).nextAll("#ils_cycle").children(":visible:first");
+                if (checkOverflow()){
+                    firstVisible.hide();
+                    $("#leftChevron").removeClass("disabled");
+                }
+                if (!checkOverflow()){
+                    $(this).addClass("disabled");
+                }
+                e.stopPropagation();
+            })
+            .on('tabClick',function (e) {
+                var clickedTab = isOutsideContainer(e);
+                if (clickedTab){
+                        $("#rightChevron").trigger('click');
+                        $(e.target).trigger("tabClick");
+
+                }
+            });
+    }
+
+    function checkOverflow(){ //checks if tab bar has still overflowing items to display (on the right)
+
+        var childrenWidth=0;
+        ils_cycle_bar.children(':visible').each(function(){
+            childrenWidth+=$(this).width();
+        });
+        var margin=3;
+        if (childrenWidth-margin > ils_cycle_bar.width()) {
+        return true;
+        }else return false;
+    }
+
+    function hasHidden(elem){ // checks if tab bar has hidden  items to display (on the left)
+        var hiddenElem=$(elem).nextAll("#ils_cycle").children(":hidden:last");
+        if (hiddenElem[0]!=null && hiddenElem[0]!=undefined&hiddenElem[0]!=""){
+            return hiddenElem;
+        }else return false;
+    }
+
+    function isOutsideContainer(elem){
+        var targetTab= $(elem.target).parent();
+        var targetTabWidth=targetTab.outerWidth();
+        var targetTabOffset=targetTab.position().left;
+        var containerWidth=targetTab.parents("#ils_cycle").width();
+        if ((targetTabOffset+targetTabWidth)>containerWidth) {
+            return targetTab;
+        }
+        else return false;
+    }
+}
 
 
-$("#main_block").scroll(function(){
+$(window).scroll(function(){
     stickyBar();
-});
-
-$(window).resize(function(){
-    checkTabBarOverflow();
 });
 
 
 function stickyBar() {
 
     fauxOffset=faux.offset().top;
+    windowScrollTop= $(this).scrollTop();
+    var test = fauxOffset - windowScrollTop ;
+    var test2 = fauxOffset - windowScrollTop - customBarHeight;
     myNavMenuHeight = myNavMenu.outerHeight(); //gets the height of tabbed menu
 
-    console.log(fauxOffset,$(window).scrollTop());
-
-    if ((fauxOffset < customBarHeight) && !attached ) //attach the sticky bar when it has reached to the top
+    if ((test < customBarHeight) && !attached ) //attach the sticky bar when it has reached to the top
         {
             attached=true;
             myNavMenu.addClass("fixedCustomTop").css("top",customBarHeight);
             faux.css("height", myNavMenuHeight);
          }
 
-    else   if ((fauxOffset > customBarHeight) && attached) // detach the sticky bar when not at the top
+    else   if ((test > customBarHeight) && attached) // detach the sticky bar when not at the top
 
     {
         myNavMenu.removeClass("fixedCustomTop").css("top",0);
@@ -94,85 +161,6 @@ var animate_logo = function(){
 
 
 };
-
-var checkTabBarOverflow=function(){ //Check if tab bar has overflow
-    var ils_cycle_bar=$("#ils_cycle");
-    var ils_wrapper=$('#customTopWrapper');
-    if (checkOverflow("current")&&!ils_cycle_bar.hasClass("hasChevrons")){
-        ils_cycle_bar.addClass("hasChevrons").parent("#customTopWrapper").prepend("<div id='leftChevron' class='tabChevron left disabled'/><div id='rightChevron' class='tabChevron right'/>");
-
-        $('body')
-            .on('click','#leftChevron',function (e) {
-                var elem=hasHidden(this);
-                if (elem){
-                    $("#rightChevron").removeClass("disabled");
-                    elem.show();
-                }
-                if (!hasHidden(this)){
-                    $(this).addClass("disabled");
-                }
-                e.stopPropagation();
-            })
-            .on('click','#rightChevron',function (e) {
-                var firstVisible=$(this).nextAll("#ils_cycle").children(":visible:first");
-                if (checkOverflow("current")){
-                    firstVisible.hide();
-                    $("#leftChevron").removeClass("disabled");
-                }
-                if (!checkOverflow("current")){
-                    $(this).addClass("disabled");
-                }
-                e.stopPropagation();
-            })
-            .on('tabClick',function (e) {
-                var clickedTab = isOutsideContainer(e);
-                if (clickedTab){
-                    $("#rightChevron").trigger('click');
-                    $(e.target).trigger("tabClick");
-
-                }
-            });
-    }else if (!checkOverflow("initial")&&ils_cycle_bar.hasClass("hasChevrons")) {
-        ils_cycle_bar.removeClass("hasChevrons").parent("#customTopWrapper").children(".tabChevron").remove();
-        ils_cycle_bar.children().show();
-    }
-
-    function checkOverflow(state){ //checks if tab bar has still overflowing items to display (on the right)
-
-        var childrenWidth=0;
-        if (state=="current") {
-            ils_cycle_bar.children(':visible').each(function () {
-                childrenWidth += $(this).width();
-            });
-        }else if (state=="initial") {
-            ils_cycle_bar.children().each(function () {
-                childrenWidth += $(this).width();
-            });
-        }
-        var margin=3;
-        if (childrenWidth-margin > ils_cycle_bar.width()) {
-            return true;
-        }else return false;
-    }
-
-    function hasHidden(elem){ // checks if tab bar has hidden  items to display (on the left)
-        var hiddenElem=$(elem).nextAll("#ils_cycle").children(":hidden:last");
-        if (hiddenElem[0]!=null && hiddenElem[0]!=undefined&hiddenElem[0]!=""){
-            return hiddenElem;
-        }else return false;
-    }
-
-    function isOutsideContainer(elem){
-        var targetTab= $(elem.target).parent();
-        var targetTabWidth=targetTab.outerWidth();
-        var targetTabOffset=targetTab.position().left;
-        var containerWidth=targetTab.parents("#ils_cycle").width();
-        if ((targetTabOffset+targetTabWidth)>containerWidth) {
-            return targetTab;
-        }
-        else return false;
-    }
-}
 
 
 
