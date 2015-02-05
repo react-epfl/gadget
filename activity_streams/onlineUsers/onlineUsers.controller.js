4f7a202417ba4b338af8cc51af7f59d157fd7d87
David Sandoz <david.sandoz@epfl.ch>
2015-02-05 22:50:06 +0100

    Gadget resizing after 5 seconds
    to ensure that everything is loaded when resizing.

--- activity_streams/onlineUsers/onlineUsers.controller.js
+++ activity_streams/onlineUsers/onlineUsers.controller.js
@@ -102,7 +102,9 @@ function ($scope, $timeout, Spaces) {
         $scope.notInIlsMessage = 'Please, place this app in an inquiry space to get visualise who is active in which inquiry phase.';
         $scope.$apply();
       }
-      gadgets.window.adjustHeight();
+      setTimeout(function() {
+        gadgets.window.adjustHeight();
+      }, 5000);
     });
   };
 
