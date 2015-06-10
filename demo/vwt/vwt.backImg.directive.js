1fc108bfc0f3572e4ef9ffbabac225b92cdd9068
vishnu-muthiah <vishnu3649m@gmail.com>
2015-06-10 14:30:00 +0200

    follows the client app as in the vertical-turbine-bbb/RemoteLab-Client repo

---
+++ demo/vwt/vwt.backImg.directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.directive('backImg', function(){
+  return function(scope, element, attrs){
+    var url = attrs.backImg;
+    element.css({
+      'background-image': 'url(' + url +')',
+      'background-size' : 'cover'
+    });
+  };
+});
