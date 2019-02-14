d24573ce5b5600b586ba2d293a840d9062b69317
André Nogueira <andre.nogueira@epfl.ch>
2019-02-14 16:55:54 +0100

    demo folder migrated to https

--- demo/vwt/jquery.jqplot.js
+++ demo/vwt/jquery.jqplot.js
@@ -28,8 +28,8 @@
  * 
  *     version 2007.04.27
  *     author Ash Searle
- *     http://hexmen.com/blog/2007/03/printf-sprintf/
- *     http://hexmen.com/js/sprintf.js
+ *     https://hexmen.com/blog/2007/03/printf-sprintf/
+ *     https://hexmen.com/js/sprintf.js
  *     The author (Ash Searle) has placed this code in the public domain:
  *     "This code is unrestricted: you are free to use it however you like."
  * 
@@ -373,7 +373,7 @@
     
     // canvas related tests taken from modernizer:
     // Copyright (c) 2009 - 2010 Faruk Ates.
-    // http://www.modernizr.com
+    // https://modernizr.com
     
     $.jqplot.support_canvas = function() {
         if (typeof $.jqplot.support_canvas.result == 'undefined') {
@@ -9337,7 +9337,7 @@
      * Ken Snyder (ken d snyder at gmail dot com)
      * 2008-09-10
      * version 2.0.2 (http://kendsnyder.com/sandbox/date/)     
-     * Creative Commons Attribution License 3.0 (http://creativecommons.org/licenses/by/3.0/)
+     * Creative Commons Attribution License 3.0 (https://creativecommons.org/licenses/by/3.0/)
      * </pre>
      * 
      * @class
@@ -10805,7 +10805,7 @@
      *     precision or length of the output based on arguments instead of fixed
      *     numbers.
      *
-     *     See http://perldoc.perl.org/functions/sprintf.html for more information.
+     *     See https://perldoc.perl.org/functions/sprintf.html for more information.
      *
      *     Implemented:
      *     - zero and space-padding
@@ -10827,8 +10827,8 @@
      *     @author Ash Searle 
      * 
      * You can see the original work and comments on his blog:
-     * http://hexmen.com/blog/2007/03/printf-sprintf/
-     * http://hexmen.com/js/sprintf.js
+     * https://hexmen.com/blog/2007/03/printf-sprintf/
+     * https://hexmen.com/js/sprintf.js
      */
      
      /**
